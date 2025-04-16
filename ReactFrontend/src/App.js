8b13730b90a631bade5d4d173435e81bc677163f
Legitbub <153683076+Legitbub@users.noreply.github.com>
2025-04-15 17:48:58 -0700

    Updated front page + error page; refactored update into front page

--- ReactFrontend/src/App.js
+++ ReactFrontend/src/App.js
@@ -3,50 +3,17 @@ import Button from './Buttons/Buttons.js';
 import ErrorPage from './Pages/Error404.js';
 import MainPage from './Pages/MainPage.js';
 import {Route, Routes} from 'react-router-dom';
-import { useState } from 'react';
 
 function App() {
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
-
   return (
-    <div>
-        <Routes>
-            <Route path='/' element={<MainPage />} />
-            <Route path='*' element={<ErrorPage />} />
-        </Routes>
-    <div className="App">
-      <h1 className="text-center">Hello bootybutthead!</h1>
-      <p>Aint noway I working on frontend right now :sob:.</p>
-
-      <div className="AddingAccount">
-        <h2 className="Text">Welcome</h2>
-
-        <input
-          type="text"
-          placeholder="Your Username"
-          className="text-center"
-          onChange={(event) => setUsername(event.target.value)}
-        />
-
-        <input
-          type="password"
-          placeholder="Your Password"
-          className="text-center"
-          onChange={(event) => setPassword(event.target.value)}
-        />
-
-        <button>Add Account</button>
-        <button>Forget Password?</button>
-      </div>
-
-      <ul>
-        <li>Test1</li>
-        <li>Test2</li>
-      </ul>
-
-      <Button />
-    </div>
+    <>
+        <div>
+            <Routes>
+                <Route path='/' element={<MainPage />} />
+                <Route path='*' element={<ErrorPage />} />
+            </Routes>
+        </div>
+    </>
   );
 }
 
