09157ea25c8dff94ea500a6a59b6839f2f618c46
Legitbub <153683076+Legitbub@users.noreply.github.com>
2025-04-20 22:16:22 -0700

    Backend startup edits

--- JavaBackend/src/main/java/me/authenticate/Authenticator/CS157A/Entity/ForgotRequest.java
+++ JavaBackend/src/main/java/me/authenticate/Authenticator/CS157A/Entity/ForgotRequest.java
@@ -1,12 +1,17 @@
 package me.authenticate.Authenticator.CS157A.Entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 public class ForgotRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int requestID;
     private int userID;
     private String resetToken;
